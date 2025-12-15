ec2e57426c81e7580b3d02e468e3572da7b4b84d
Vanshika Pal <vanshikapal451@gmail.com>
2025-12-15 13:32:14 +0530

    Revamp ContentView layout with new design elements

--- Food Card Zomato/ContentView.swift
+++ Food Card Zomato/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            ZStack(alignment: .topLeading) {
+                Image(.avacado)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 390, height: 250)
+                    .cornerRadius(20)
+                    .clipped()
+                Text("Non-Veg")
+                    .font(.system(size: 15))
+                    .foregroundStyle(.white)
+                    .padding(7)
+                    .background(Color.red)
+                    .cornerRadius(10)
+                    .offset(x: 10, y: 10)
+                Text("35 min")
+                    .font(.system(size: 15))
+                    .foregroundStyle(.black)
+                    .padding(5)
+                    .background(.white.opacity(0.6))
+                    .cornerRadius(10)
+                    .offset(x: 325, y: 210)
+            }
+            .shadow(radius: 5)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Avacado Egg Toast")
+                        .font(.system(size: 30))
+                        .foregroundStyle(.black)
+                    Label("4.2", systemImage: "star.fill")
+                        .foregroundStyle(.white)
+                        .padding(5)
+                        .background(.green)
+                        .cornerRadius(10)
+                        .offset(x: 40)
+                }
+                .offset(y: -10)
+                HStack {
+                    Text("""
+                        Avocado toast topped with
+                        sliced boiled egg.
+                        """)
+                    .foregroundStyle(.black.opacity(0.9))
+                    Text("₹250")
+                        .font(.system(size: 20))
+                        .foregroundStyle(.secondary)
+                        .offset(x: 90)
+                }
+                .offset(y: -10)
+                
+            }
+            
+            .padding()
         }
-        .padding()
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 30)
     }
 }
 
