f3b440aef1f858065ffb0aa38e685535461e6b94
Vanshika Pal <vanshikapal451@gmail.com>
2025-12-15 13:59:57 +0530

    Add pizza card and details to ContentView

--- Food Card Zomato/ContentView.swift
+++ Food Card Zomato/ContentView.swift
@@ -66,6 +66,67 @@ struct ContentView: View {
         .background(Color.white)
         .cornerRadius(20)
         .shadow(radius: 30)
+
+        VStack(alignment: .leading) {
+            ZStack(alignment: .topLeading) {
+                Image(.pizza)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 390, height: 250)
+                    .cornerRadius(20)
+                    .clipped()
+                Text("Non-Veg")
+                    .font(.system(size: 15))
+                    .foregroundStyle(.white)
+                    .padding(7)
+                    .background(Color.red)
+                    .cornerRadius(10)
+                    .offset(x: 10, y: 10)
+                Text("40 min")
+                    .font(.system(size: 15))
+                    .foregroundStyle(.black)
+                    .padding(5)
+                    .background(.white.opacity(0.6))
+                    .cornerRadius(10)
+                    .offset(x: 325, y: 210)
+            }
+            .shadow(radius: 5)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("BBQ Chicken Pizza")
+                        .font(.system(size: 30))
+                        .foregroundStyle(.black)
+                    Label("4.4", systemImage: "star.fill")
+                        .foregroundStyle(.white)
+                        .padding(5)
+                        .background(.green)
+                        .cornerRadius(10)
+                        .offset(x: 40)
+                }
+                .offset(y: -10)
+                HStack {
+                    Text("""
+                        BBQ chicken pizza topped with pineapple,
+                        melted cheese, and 
+                        fresh herbs.
+                        """)
+                    .foregroundStyle(.black.opacity(0.9))
+                    Text("₹499")
+                        .font(.system(size: 20))
+                        .foregroundStyle(.secondary)
+                        .offset(x: 90)
+                }
+                .offset(y: -10)
+                
+            }
+            
+            .padding()
+            
+            
+        }
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 30)
     }
 }
 
