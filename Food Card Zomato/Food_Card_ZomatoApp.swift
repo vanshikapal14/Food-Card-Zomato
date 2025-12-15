e13ed0a9baa86b6afd20b5ce06c034c5f14ecc60
Vanshika Pal <vanshikapal451@gmail.com>
2025-12-15 12:25:39 +0530

    Initial Commit

---
+++ Food Card Zomato/Food_Card_ZomatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Card_ZomatoApp.swift
+//  Food Card Zomato
+//
+//  Created by Vanshika Pal on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Food_Card_ZomatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
